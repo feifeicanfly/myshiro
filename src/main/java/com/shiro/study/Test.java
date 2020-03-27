bff518e58e0d0fb0ee6196c6226072e340438b50
feifeicanfly <raner1218@163.com>
2020-03-27 17:01:07 +0800

    简单测试

---
+++ src/main/java/com/shiro/study/Test.java
@@ -0,0 +1,12 @@
+package com.shiro.study;
+
+/**
+ * @description: TODO<尽量简短描述其作用>
+ * @author: Zhuojc
+ * @create: 2020-03-27 17:00
+ */
+public class Test {
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
+}
